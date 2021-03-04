aaa9d478baa3bddaee95aadc1df6b53cc1073025
Alina-dev-front <kolmykova93@gmail.com>
2021-03-04 14:25:15 +0100

    add getById func

--- main.go
+++ main.go
@@ -30,53 +30,12 @@ type Shipment struct {
 	Price                float64 `json:"Price"`
 }
 
-// type allShipments []shipment
-
-// var shipments = allShipments{
-// 	{
-// 		ID:                   "1",
-// 		SenderName:           "Nilsson, Tim",
-// 		SenderEmail:          "",
-// 		SenderAddress:        "",
-// 		SenderCountryCode:    "",
-// 		RecipientName:        "Jack",
-// 		RecipientEmail:       "",
-// 		RecipientAddress:     "",
-// 		RecipientCountryCode: "",
-// 		Weight:               0.451,
-// 		Price:                0,
-// 	},
-// }
-
-func addShipment(w http.ResponseWriter, r *http.Request) {
-
-	insert, err := DB.Query("insert into `Shipments` (`SenderName`) values('Dick Harrison')")
-	if err != nil {
-		panic(err)
-	}
-
-	defer insert.Close()
-}
-
-// func getOneShipment(w http.ResponseWriter, r *http.Request) {
-// 	shipmentID := mux.Vars(r)["id"]
-
-// 	for _, singleShipment := range shipments {
-// 		if singleShipment.ID == shipmentID {
-// 			json.NewEncoder(w).Encode(singleShipment)
-// 		}
-// 	}
-// }
-
 func getAllShipments(w http.ResponseWriter, r *http.Request) {
 
 	rows, err := DB.Query("SELECT * FROM `Shipments`")
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println(rows)
-
 	defer rows.Close()
 
 	var allShipments []*Shipment
@@ -85,30 +44,52 @@ func getAllShipments(w http.ResponseWriter, r *http.Request) {
 		err := rows.Scan(&s.ID, &s.SenderName, &s.SenderEmail, &s.SenderAddress, &s.SenderCountryCode, &s.RecipientName, &s.RecipientEmail, &s.RecipientAddress, &s.RecipientCountryCode, &s.Weight, &s.Price)
 		if err != nil {
 			fmt.Println(err)
-			// return err
+			return
 		}
-
 		allShipments = append(allShipments, s)
 	}
 
 	if err := rows.Err(); err != nil {
 		fmt.Println(err)
-		// return err
+		return
 	}
 
 	if err := json.NewEncoder(w).Encode(allShipments); err != nil {
 		fmt.Println(err)
 	}
+}
 
-	// var jsonData, erro1 = json.Marshal(rows)
-	// if erro1 != nil {
-	// 	log.Println(erro1)
-	// }
+func getOneShipment(w http.ResponseWriter, r *http.Request) {
 
-	// fmt.Println(string(jsonData))
-	// json.NewEncoder(w).Encode(jsonData)
+	shipmentID := mux.Vars(r)["id"]
 
-	fmt.Println(allShipments)
+	result, err := DB.Query("SELECT * FROM `Shipments` WHERE id = ?", shipmentID)
+	if err != nil {
+		panic(err)
+	}
+
+	defer result.Close()
+
+	var s Shipment
+
+	for result.Next() {
+		err := result.Scan(&s.ID, &s.SenderName, &s.SenderEmail, &s.SenderAddress, &s.SenderCountryCode, &s.RecipientName, &s.RecipientEmail, &s.RecipientAddress, &s.RecipientCountryCode, &s.Weight, &s.Price)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	json.NewEncoder(w).Encode(s)
+}
+
+func addShipment(w http.ResponseWriter, r *http.Request) {
+
+	insert, err := DB.Query("INSERT INTO `Shipments` (`SenderName`) VALUES('Dick Harrison')")
+	if err != nil {
+		panic(err)
+	}
+
+	defer insert.Close()
 }
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
@@ -128,7 +109,7 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/shipment", addShipment).Methods("POST")
-	//router.HandleFunc("/shipments/{id}", getOneShipment).Methods("GET")
+	router.HandleFunc("/shipments/{id}", getOneShipment).Methods("GET")
 	router.HandleFunc("/shipments", getAllShipments).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
